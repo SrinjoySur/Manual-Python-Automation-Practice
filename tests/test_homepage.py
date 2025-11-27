2b660c41ccf4c8349f41b7e57ecadd8a5cde5786
Srinjoy Sur <srinjoy_sur@epam.com>
2025-11-27 14:38:09 +0530

    chore: test navigation from homepage to products page

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,6 +1,7 @@
 import re
 from playwright.sync_api import expect
 from pages.homepage import HomePage
+from pages.productspage import ProductsPage
 class Test_HomePage_tests:
     def test_logoVisibility(self,page):
         home_page=HomePage(page)
@@ -10,3 +11,10 @@ class Test_HomePage_tests:
             expect(element).to_be_visible()
         except:
             TimeoutError()
+    def test_navigateToProductsPage(self,page):
+        home_page=HomePage(page)
+        home_page.navigate()
+        home_page.clickProductsPageLink()
+        products_page=ProductsPage(page)
+        title=products_page.getTitle()
+        assert re.search("Automation Exercise - All Products",title)
